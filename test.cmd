9c4e093293eeb8c6bef1e44f23fca4ad25aa4288
flameSla <39356103+flameSla@users.noreply.github.com>
2022-05-01 09:42:41 +0700

    Update test.cmd

--- test.cmd
+++ test.cmd
@@ -1,4 +1,5 @@
 python decode-export-string.py < tests\win\bp.txt | python encode-export-string.py > tests\win\new_bp.txt
 python decode-export-string.py < tests\win\bp.txt > tests\win\json.txt
+python encode-export-string.py < tests\win\json.txt > tests\win\export.txt
 fc /b tests\win\bp.txt tests\win\new_bp.txt
 pause
